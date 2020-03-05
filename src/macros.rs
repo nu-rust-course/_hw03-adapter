970c9f40b5cc24f2822fd8a8e76e33ec8d2767ad
Jesse A. Tov <jesse.tov@gmail.com>
2020-03-05 11:41:29 -0600

    Spell "adapter" like this.

--- src/macros.rs
+++ src/macros.rs
@@ -105,7 +105,7 @@ macro_rules! adapt {
         #[allow(missing_docs)]
         mod adaptations {
             use super::*;
-            use ::hw03_adaptor as m;
+            use ::hw03_adapter as m;
             $($extra)*
             m::convert_correction!( $($correction_toks)* );
             m::adapt_corrector!( $($corrector_toks)* );
